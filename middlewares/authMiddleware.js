a4cfc90ddaf473d255735602b860a5b7258d55a9
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-11-21 09:30:15 +0700

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,25 @@
-module.exports = (req, res, next) => {
+export default  (req, res, next) => {
     if (!req.user) {
         return res.status(401).send('Not authorized');
     }
     next(); 
 };
+
+// protect routes middleware function
+export const protectedRoute = (req, res, next) => {
+    if (!req.session.user) {
+      return res.redirect('/login');
+    }
+    next();
+  }
+  
+  // guest routes middleware function
+  export const guestRoute = (req, res, next) => {
+    if (req.session.user) {
+      return res.redirect('/profile');
+    }
+    next();
+  }
+
+
+
