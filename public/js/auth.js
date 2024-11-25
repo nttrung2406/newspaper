3d27e5879ac10d0b6b30e70184168c8470172008
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-11-25 15:19:09 +0700

    Add files via upload

---
+++ public/js/auth.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginBtn = document.getElementById("login-btn");
+    const signupBtn = document.getElementById("signup-btn");
+    const loginForm = document.getElementById("login-form");
+    const signupForm = document.getElementById("signup-form");
+
+    loginBtn.addEventListener("click", function () {
+        loginForm.classList.remove("hidden");
+        signupForm.classList.add("hidden");
+        loginBtn.classList.add("active");
+        signupBtn.classList.remove("active");
+    });
+
+    signupBtn.addEventListener("click", function () {
+        signupForm.classList.remove("hidden");
+        loginForm.classList.add("hidden");
+        signupBtn.classList.add("active");
+        loginBtn.classList.remove("active");
+    });
+});
