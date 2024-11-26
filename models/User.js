1e1d8a5e30c4d140d59bdf4646be875aa94af0ea
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-11-26 15:58:09 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    // Add other fields as needed, e.g., email, role, etc.
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum: ['membership', 'editor', 'writer', 'administrator'] },
+    createdAt: { type: Date, default: Date.now },
 });
 
-UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
