a49ba3fce066906f3bb0a35f92c9e226b429da3a
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-11-26 15:59:23 +0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,13 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  token: String,
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-  posts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post',
-  }]
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum: ['membership', 'editor', 'writer', 'administrator'] },
+    createdAt: { type: Date, default: Date.now },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
 export default User;
