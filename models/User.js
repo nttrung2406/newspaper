7cd98e62899658cc6abe05ff3dbcf7de9b5bb380
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-11-27 16:51:25 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,24 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, required: true, enum: ['membership', 'editor', 'writer', 'administrator'] },
-    createdAt: { type: Date, default: Date.now },
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, 
+  role: {
+    type: String,
+    enum: ['Membership', 'Writer', 'Editor', 'Admin'],
+    required: true
+  },
+  membership: {
+    expiryDate: { type: Date }, // For Membership role only
+    categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }] // Allowed categories
+  },
+  writerPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }], // For Writer role
+  editorPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }], // For Editor role
+}, {
+  timestamps: true 
 });
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
