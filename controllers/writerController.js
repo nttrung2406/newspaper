93c2d20ec5cdb3e0fb180068b1d61c7c642028a4
TIN PHAM <ptbtin22@clc.fitus.edu.vn>
2024-12-03 22:17:21 +0700

    seperate routing logic from controller logic + fix public folder

--- controllers/writerController.js
+++ controllers/writerController.js
@@ -1,14 +1,14 @@
 const writerController = {
-    getEditPost: (req, res, next) => {
-      res.render("edit-post", {
-        pageTitle: "Edit Post",
-        path: "writer/edit-post",
-      });
-    },
-    postEditPost: (req, res, next) => {
-      const { title, detail, category, tags } = req.body;
-      console.log(title, detail, category, tags);
-    },
-  };
-  
-  export default writerController;
+  getEditPost: (req, res, next) => {
+    res.render("edit_post", {
+      pageTitle: "Edit Post",
+      path: "writer/edit-post",
+    });
+  },
+  postEditPost: (req, res, next) => {
+    const { title, detail, category, tags } = req.body;
+    console.log(title, detail, category, tags);
+  },
+};
+
+export default writerController;
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import flash from 'connect-flash';
-import connectMongoDB from './db.js';
-import authRoutes from './routes/authRoutes.js';
-import postRoutes from './routes/postRoutes.js';
-import authMiddleware from './middlewares/authMiddleware.js';
-import userRoutes from './routes/userRoutes.js';
-import setUserData from './middlewares/setUserData.js';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import flash from "connect-flash";
+import connectMongoDB from "./db.js";
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import authMiddleware from "./middlewares/authMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import setUserData from "./middlewares/setUserData.js";
 import writerRoutes from "./routes/writerRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js"
+import adminRoutes from "./routes/adminRoutes.js";
+import { dirname } from "path";
 
-dotenv.config({ path: './config/env/development.env' });
+dotenv.config({ path: "./config/env/development.env" });
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -25,102 +26,72 @@ const testDatabaseConnection = async () => {
   try {
     const db = mongoose.connection;
     const collections = await db.db.listCollections().toArray();
-    console.log('Collections in database:', collections.map(c => c.name));
+    console.log(
+      "Collections in database:",
+      collections.map((c) => c.name)
+    );
   } catch (err) {
-    console.error('Error fetching collections:', err.message);
+    console.error("Error fetching collections:", err.message);
   }
 };
 
-connectMongoDB().then(testDatabaseConnection);
+// connectMongoDB().then(testDatabaseConnection);
 
 // Resolve __dirname equivalent in ESM
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(session({
-  secret: 'secret',  // Replace with a strong secret key for session encryption
-  resave: false,              // Don't resave session if it hasn't changed
-  saveUninitialized: true,    // Save a session even if it is new and hasn't been modified
-  cookie: {
-      httpOnly: true,         // Security measure: prevent access to cookie via JavaScript
-      secure: false,          // If using https, set to true; for development, set to false
-      maxAge: 1000 * 60 * 60 * 24 // Set the session expiration time (optional, here it's 1 day)
-  }
-}));
+app.use(
+  session({
+    secret: "secret", // Replace with a strong secret key for session encryption
+    resave: false, // Don't resave session if it hasn't changed
+    saveUninitialized: true, // Save a session even if it is new and hasn't been modified
+    cookie: {
+      httpOnly: true, // Security measure: prevent access to cookie via JavaScript
+      secure: false, // If using https, set to true; for development, set to false
+      maxAge: 1000 * 60 * 60 * 24, // Set the session expiration time (optional, here it's 1 day)
+    },
+  })
+);
 app.use(flash());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', authMiddleware);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", authMiddleware);
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/posts', postRoutes);
-app.use('/users', userRoutes);
-app.use('/admin', adminRoutes);
-
-
-// Static files
-app.use('/assets', express.static(path.join(__dirname, 'public')));
-app.use('/', express.static(path.join(__dirname, 'views')));
+app.use("/auth", authRoutes);
+app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
+app.use("/admin", adminRoutes);
+app.use("/writer", writerRoutes);
 
 // Use user
 app.use(setUserData);
 
 // Pages
-app.get('/', (req, res) => res.render('index'));
-app.get('/index', (req, res) => res.render('index'));
-app.get('/categori', (req, res) => res.render('categori'));
-app.get('/about', (req, res) => res.render('about'));
-app.get('/latest_news', (req, res) => res.render('latest_news'));
-app.get('/contact', (req, res) => res.render('contact'));
-app.get('/elements', (req, res) => res.render('elements'));
-app.get('/blog', (req, res) => res.render('blog'));
-app.get('/single-blog', (req, res) => res.render('single-blog'));
-app.get('/details', (req, res) => res.render('details'));
-app.get('/auth', (req, res) => res.render('auth'));
-app.get('/forgot_password', (req, res) => res.render('forgot_password'));
-app.get('/reset_password', (req, res) => {
-  const { token } = req.query;
-  res.render('reset_password', { token });
-});
+app.get("/", (req, res) => res.render("index"));
+app.get("/index", (req, res) => res.render("index"));
+app.get("/categori", (req, res) => res.render("categori"));
+app.get("/about", (req, res) => res.render("about"));
+app.get("/latest_news", (req, res) => res.render("latest_news"));
+app.get("/contact", (req, res) => res.render("contact"));
+app.get("/elements", (req, res) => res.render("elements"));
+app.get("/blog", (req, res) => res.render("blog"));
+app.get("/single-blog", (req, res) => res.render("single-blog"));
+app.get("/details", (req, res) => res.render("details"));
 
-app.use("/writer", writerRoutes);
-
-app.get('/profile', (req, res) => {
-  if (!req.session.user) {
-    return res.redirect('/auth'); 
-  }
-  res.render('profile', { user: req.session.user });
-});
-
-app.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-      if (err) {
-          console.error('Error destroying session:', err);
-          return res.status(500).send('Logout failed.');
-      }
-      res.clearCookie('connect.sid'); 
-      res.redirect('/index'); 
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    console.log("Connected");
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
-
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username, password });
-  if (user) {
-    req.session.user = { username: user.username }; 
-    res.redirect('/profile');
-  } else {
-    res.redirect('/auth'); 
-  }
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
