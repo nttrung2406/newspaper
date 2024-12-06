484e04f4bd505646a15eec753f3dc25558248488
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-12-06 17:54:00 +0700

    fx

--- utils/authHelpers.js
+++ utils/authHelpers.js
@@ -26,6 +26,8 @@ export const transporter = nodemailer.createTransport({
     // service: "gmail",
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: true,
+    requireTLS: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
