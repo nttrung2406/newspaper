4661b8ebfebadae50c1c02fda7b3f3bf27517411
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-12-06 17:06:14 +0700

    fix

--- utils/authHelpers.js
+++ utils/authHelpers.js
@@ -23,8 +23,8 @@ export const validateResetToken = (token) => {
 // Nodemailer transporter
 export const transporter = nodemailer.createTransport({
     service: "gmail",
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    // host: process.env.SMTP_HOST,
+    // port: process.env.SMTP_PORT,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
