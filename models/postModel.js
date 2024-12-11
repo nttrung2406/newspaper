a8c0e51057131efb0058f6979bd07fc60905f495
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-12-11 15:06:48 +0700

    fix approve

--- models/postModel.js
+++ models/postModel.js
@@ -6,22 +6,21 @@ const postSchema = new mongoose.Schema({
   slug: { type: String, required: true, unique: true },
   status: {
     type: String,
-    enum: ['Draft', 'Submitted', 'Approved', 'Rejected'], // Approval workflow
+    enum: ['Draft', 'Submitted', 'Approved', 'Rejected'], 
     default: 'Draft'
   },
-  writer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Writer who created the post
-  editor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Editor who approved/rejected
-  rejectionReason: { type: String }, // Reason for rejection (if rejected)
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }, // Category of the article
-  tags: [String], // Tags associated with the article
-  createdAt: { type: Date }, // When the article is scheduled to be published
-  updatedAt: { type: Date }, // When the article's details are updated
+  writer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+  editor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+  rejectionReason: { type: String }, 
+  category: { type: String, ref: 'Category' }, 
+  tags: [String], 
+  createdAt: { type: Date }, 
+  updatedAt: { type: Date }, 
 }, 
 {
   timestamps: true // Automatically adds createdAt and updatedAt fields
 });
 
-// Create the model
 const Post = mongoose.model('Post', postSchema);
 
 export default Post;
