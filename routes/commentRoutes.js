79feff74a5effcba59e6dbffa724454b10a1b6b9
kindinh903 <kindinh903@gmail.com>
2024-12-28 04:59:03 +0700

    delete comment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getCommentsByPostId, addComment } from '../controllers/commentController.js';
+import { getCommentsByPostId, addComment, deleteComment } from '../controllers/commentController.js';
 const router = express.Router();
 
 // Lấy tất cả comment theo postId
@@ -7,5 +7,6 @@ router.get('/:postId', getCommentsByPostId);
 
 // Thêm comment mới
 router.post('/', addComment);
+router.delete('/:id', deleteComment);       // Xóa bình luận
 
 export default router;
