3a9b3a1885bf63d9af56470ea47bc6c6fe6defbc
kindinh903 <kindinh903@gmail.com>
2024-12-28 04:13:04 +0700

    comment with username

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,8 +1,11 @@
 import Comment from '../models/commentModel.js';
-
+import User from '../models/User.js';
+import setUserData from '../middlewares/setUserData.js';
 export const getCommentsByPostId = async (req, res) => {
     try {
-        const comments = await Comment.find({ postId: req.params.postId }).sort({ createdAt: -1 });
+        const comments = await Comment.find({ postId: req.params.postId })
+            .populate('userId', 'username')  // Lấy trường username từ bảng users
+            .sort({ createdAt: -1 });
         res.json(comments);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch comments' });
@@ -11,8 +14,12 @@ export const getCommentsByPostId = async (req, res) => {
 
 export const addComment = async (req, res) => {
     try {
-        const { postId, author, content } = req.body;
-        const newComment = new Comment({ postId, author, content });
+        if (!req.session.user){
+            return res.status(401).json({ error: 'Please log in to comment.', redirect: '/login' });
+        }
+        const userId = req.session.user._id;
+        const { postId, content } = req.body;
+        const newComment = new Comment({ postId, userId, content });
         await newComment.save();
         res.status(201).json(newComment);
     } catch (error) {
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { getCommentsByPostId, addComment } from '../controllers/commentController.js';
-
 const router = express.Router();
 
 // Lấy tất cả comment theo postId
