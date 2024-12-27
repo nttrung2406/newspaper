11f568adfe68189be8cc92c5b2b1550251fe8458
kindinh903 <kindinh903@gmail.com>
2024-12-27 20:39:49 +0700

    fix authorize membership

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -37,31 +37,39 @@ export const authorizeRole = (allowedRoles) => {
       req.flash('error', 'You must be logged in to access this page.');
       return res.redirect('/auth'); 
     }
-    const membership = req.session.user.membership;
     // Check if the user's role is in the allowed roles
     const userRole = req.session.user.role;
     if (!allowedRoles.includes(userRole)) {
       req.flash('error', 'You do not have permission to access this page.');
       return res.redirect('/categori'); 
     }
-    // console.log(req.session.user.membership);
-    // if (membership.type==='basic') {
-    //   req.flash('error', 'Access Denied: Youre accessing the premium only content.');
-    //   return res.redirect('/categori'); 
-    // }
-    // else if (membership.endDate < Date.now()) {
-    //   req.flash('error', 'Access Denied: Your membership has expired.');
-    //   return res.redirect('/categori'); 
-    // }
-    // else if (membership.status === 'inactive') {
-    //   req.flash('error', 'Access Denied: Please contact us to activate youre premium access.');
-    //   return res.redirect('/categori'); 
-    // }
-
     next();
   };
 };
 
-
+export const authorizeMembership = (allowedMemberships) => {
+  return (req, res, next) => {
+    // Check if user is logged in
+    if (!req.session || !req.session.user) {
+      req.flash('error', 'You must be logged in to access this page.');
+      return res.redirect('/auth'); 
+    }
+    const membership = req.session.user.membership;
+    console.log(membership);
+    if (membership.type==='basic') {
+      req.flash('error', 'Access Denied: Youre accessing the premium only content.');
+      return res.redirect('/categori'); 
+    }
+    else if (membership.endDate < Date.now()) {
+      req.flash('error', 'Access Denied: Your membership has expired.');
+      return res.redirect('/categori'); 
+    }
+    else if (membership.status === 'inactive') {
+      req.flash('error', 'Access Denied: Please contact us to activate youre premium access.');
+      return res.redirect('/categori'); 
+    }
+    next();
+  };
+}
 
 
