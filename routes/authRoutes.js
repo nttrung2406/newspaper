6055716bd4b5199e490a45f85ee48b633c8668fd
TIN PHAM <ptbtin22@clc.fitus.edu.vn>
2025-01-01 11:11:17 +0700

    add feature 'change password' for user with express validator for validating input

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,46 @@
 import express from "express";
 const router = express.Router();
+import { body } from "express-validator";
+import bcrypt from "bcryptjs";
 
 import authController from "../controllers/authController.js";
 
 router.get("/", authController.getAuth);
 
 router.get("/profile", authController.getProfile);
+
+router.get("/profile/edit-password", authController.getEditPassword);
+
+router.post(
+  "/profile/edit-password",
+  [
+    body("password").custom(async (value, { req }) => {
+      const isMatch = await bcrypt.compare(value, req.user.password);
+      if (!isMatch) {
+        return Promise.reject("Incorrect password! Please try again.");
+      }
+    }),
+    body("newPassword")
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage("Password must be at least 5 characters long")
+      .custom((value, { req }) => {
+        if (value === req.body.password) {
+          throw new Error("This one is already in use. Please choose another.");
+        }
+        return true;
+      }),
+    body("confirmPassword")
+      .trim()
+      .custom(async (value, { req }) => {
+        if (value !== req.body.newPassword) {
+          throw new Error("Passwords have to match!");
+        }
+        return true;
+      }),
+  ],
+  authController.postEditPassword
+);
 
 router.post("/logout", authController.postLogout);
 
@@ -15,7 +50,7 @@ router.post("/signup", authController.postSignup);
 // Login functionality
 router.post("/login", authController.postLogin);
 
-router.post('/profile/update', authController.postUpdateProfile);
+router.post("/profile/update", authController.postUpdateProfile);
 
 // Forgot password functionality
 router.post("/forgot-password", authController.postForgotPassword);
