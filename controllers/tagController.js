6a44edc22f2dda6e6172292efcc704b6ee4f8893
TIN PHAM <ptbtin22@clc.fitus.edu.vn>
2025-01-01 17:25:30 +0700

    add pagination for viewing posts by tag id

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -2,14 +2,31 @@ import Tag from "../models/Tag.js";
 import Post from "../models/postModel.js";
 import mongoose from "mongoose";
 
+const POSTS_PER_PAGE = 10;
+
 const tagController = {
   getPostsByTag: async (req, res, next) => {
+    const page = Math.max(1, +req.query.page || 1);
     const tagId = req.params.tagId;
     try {
       if (!mongoose.Types.ObjectId.isValid(tagId)) {
         throw new Error("Invalid Tag ID");
       }
-      const posts = await Post.find({ tags: tagId }).populate("tags");
+      const totalPosts = await Post.countDocuments({ tags: tagId });
+
+      const lastPage =
+        totalPosts > 0 ? Math.ceil(totalPosts / POSTS_PER_PAGE) : 1;
+
+      if (page > lastPage) {
+        return res.status(404).redirect(`/tags/${tagId}?page=${lastPage}`);
+      }
+
+      const posts = await Post.find({ tags: tagId })
+        .sort({ createdAt: -1 })
+        .skip((page - 1) * POSTS_PER_PAGE)
+        .limit(POSTS_PER_PAGE)
+        .populate("tags");
+
       const updatedPosts = posts.map((post) => {
         const match = post.content.match(/<img[^>]*src="([^"]+)"[^>]*>/);
         let imageUrl = match ? match[1] : null;
@@ -31,6 +48,12 @@ const tagController = {
       res.render("tags", {
         pageTitle: "Post by Tag",
         posts: updatedPosts,
+        currentPage: page,
+        hasNextPage: POSTS_PER_PAGE * page < totalPosts,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: lastPage,
       });
     } catch (err) {
       const error = new Error(err);
@@ -67,7 +90,5 @@ export const getAllTagsPaginated = async (req, res, next) => {
     next(error);
   }
 };
-
-
 
 export default tagController;
